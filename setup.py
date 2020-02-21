86994b2355dc8b9cbabc2f1faba717810ec611c3
Nathan Giusti <bigbadbyte2@gmail.com>
2020-02-21 14:24:51 -0600

    v 0.0.1

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PySenseSDK",
+    version="0.0.1",
+    author="Nathan Giusti",
+    author_email="nathanggiusti@gmail.com",
+    description="Sisense Python SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nathangiusti/PySense",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
