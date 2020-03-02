f7989562d67bbbabe2443b07a1cd43cb64b6b4f5
Nathan Giusti <bigbadbyte2@gmail.com>
2020-03-02 14:26:30 -0500

    removed dist/lib files

--- build/lib/Source/PySenseAlert.py
+++
@@ -1,9 +0,0 @@
-
-def createAlert(user, name):
-    alert = {}
-    alert['enabled'] = 'true'
-    alert['name'] = name
-    user = {}
-    user['type'] = "user"
-    user['id'] = 'temp'
-    alert['parties'] = 'temp'
